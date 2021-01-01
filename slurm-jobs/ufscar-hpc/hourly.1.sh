23c8c9e0eb1f95bfb684ccada6827c69db270af7
Paulo Matias <matias@ufscar.br>
2021-01-01 12:02:03 -0300

    Refactor common code in `common.sh`

--- slurm-jobs/ufscar-hpc/hourly.1.sh
+++ slurm-jobs/ufscar-hpc/hourly.1.sh
@@ -9,18 +9,4 @@
 JOB_PERIOD=3600
 JOB_OFFSET=0
 
-_requeue() {
-  local now timespec
-  now="$(date '+%s')"
-  timespec="$(date --date="@$(((now/JOB_PERIOD+1)*JOB_PERIOD+JOB_OFFSET))" '+%Y-%m-%dT%H:%M:%S')"
-
-  echo "$(date): requeing job $SLURM_JOBID ($SLURM_JOB_NAME) to run at $timespec"
-
-  scontrol requeue "$SLURM_JOBID"
-  scontrol update JobId="$SLURM_JOBID" StartTime="$timespec"
-}
-trap '_requeue' SIGUSR1 EXIT HUP INT QUIT TERM ERR
-
-echo "$(date): job $SLURM_JOBID ($SLURM_JOB_NAME) starting on $SLURM_NODELIST"
-
-chaotic routine "$SLURM_JOB_NAME"
+source "$(dirname "$0")"/common.sh
