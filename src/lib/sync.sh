6c5ae3a1ee847b1ce4c58f76940462848b6cd846
Paulo Matias <matias@ufscar.br>
2021-01-01 23:28:09 -0300

    Implement repoctl-sync-db

--- src/lib/sync.sh
+++ src/lib/sync.sh
@@ -22,5 +22,28 @@ function package-lists-sync() {
 
 function repoctl-sync-db() {
   set -euo pipefail
+
+  [[ -z "${CAUR_REPOCTL_DB_URL:-}" ]] && return 0
+  [[ -z "${CAUR_REPOCTL_DB_FILE:-}" ]] && return 0
+
+  local repoctl_config
+
+  install -o"$(whoami)" -dDm755 "${HOME}/.config/repoctl"
+  repoctl_config="${HOME}/.config/repoctl/config.toml"
+
+  if [[ -e "${repoctl_config}" ]]; then
+    if [[ "$(cat "${repoctl_config}" | wc -l)" -gt 2 ]]; then
+      echo "sanity check: we are not going to overwrite existing repoctl config"
+      return 31
+    fi
+  fi
+
+  install -o"$(whoami)" -dDm755 "$(basename "$CAUR_REPOCTL_DB_FILE")"
+  curl -s -o "${CAUR_REPOCTL_DB_FILE}" "${CAUR_REPOCTL_DB_URL}"
+
+  stee "${repoctl_config}" <<EOF
+[profiles.default]
+repo = "${CAUR_REPOCTL_DB_FILE}"
+EOF
 }
 
