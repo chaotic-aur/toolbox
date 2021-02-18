71f74e3a1686d54a25e72b145f5d1004189cab8c
dr460nf1r3 <njcrypted@protonmail.com>
2021-02-18 19:16:26 +0100

    Update permissions

--- src/lib/mirror.sh
+++ src/lib/mirror.sh
@@ -9,7 +9,7 @@ function help-mirror() {
   'syncthing')
     echo "Sorry, I haven't automated this yet!"
     echo '1) Install syncthing'
-    echo '2) Add my device: ZDHVMSP-EW4TMWX-DBH2W4P-HV5A6OY-BBEFABO-QTENANJ-RJ6GKNX-6KCG7QY'
+    echo '2) Add my device: ZDHVMSP-EW4TMWX-DBH2W4P-HV5A6OY-BBEFABO-QTENANJ-RJ6GKNX-6KCG7QY, then check "Introducer" and "Auto accept"'
     echo '3) Add folder "jhcrt-m2dra" as "Receive Only" with "Ignore Permissions" and "Pull Order" by "Oldest First".'
     echo '4) Wait for me to accept it!'
     ;;
