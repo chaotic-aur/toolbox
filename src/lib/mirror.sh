e8fe4335e94a8a52df57fa65ebfd7f69cd85b770
Nico <njcrypted@protonmail.com>
2021-02-18 18:41:47 +0000

    Update Syncthing device ID (#17)
    * Update Syncthing device ID
    
    * Update permissions
    
    * Remove rsync

--- src/lib/mirror.sh
+++ src/lib/mirror.sh
@@ -9,15 +9,9 @@ function help-mirror() {
   'syncthing')
     echo "Sorry, I haven't automated this yet!"
     echo '1) Install syncthing'
-    echo '2) Add my device: 4HGSY2A-JLFMQFF-GZWBRJP-DHICO4G-55SJODJ-7E2OHCR-M4Y7EJ3-2I4YOAJ'
+    echo '2) Add my device: ZDHVMSP-EW4TMWX-DBH2W4P-HV5A6OY-BBEFABO-QTENANJ-RJ6GKNX-6KCG7QY, then check "Introducer" and "Auto accept"'
     echo '3) Add folder "jhcrt-m2dra" as "Receive Only" with "Ignore Permissions" and "Pull Order" by "Oldest First".'
     echo '4) Wait for me to accept it!'
-    ;;
-  'rsync')
-    echo "Sorry, I haven't automated this yet!"
-    echo '1) Install rsync'
-    echo '2) Run this script once: https://gist.github.com/BangL/86c2700e169994bc147ebf076fcb1888'
-    echo '3) Schedule it to run each 15 minutes'
     ;;
   *)
     echo 'Unsupported protocol.'
