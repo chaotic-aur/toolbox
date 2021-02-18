868ed757377c74f99ffaa81ae3bc99f5344937e8
dr460nf1r3 <njcrypted@protonmail.com>
2021-02-18 19:19:37 +0100

    Remove rsync

--- src/lib/mirror.sh
+++ src/lib/mirror.sh
@@ -13,12 +13,6 @@ function help-mirror() {
     echo '3) Add folder "jhcrt-m2dra" as "Receive Only" with "Ignore Permissions" and "Pull Order" by "Oldest First".'
     echo '4) Wait for me to accept it!'
     ;;
-  'rsync')
-    echo "Sorry, I haven't automated this yet!"
-    echo '1) Install rsync'
-    echo '2) Run this script once: https://gist.github.com/BangL/86c2700e169994bc147ebf076fcb1888'
-    echo '3) Schedule it to run each 15 minutes'
-    ;;
   *)
     echo 'Unsupported protocol.'
     ;;
