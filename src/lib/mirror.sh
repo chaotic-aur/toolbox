76fd637a88268b4d0f2f4cda0234cb4b37ffa611
dr460nf1r3 <njcrypted@protonmail.com>
2021-02-18 19:11:01 +0100

    Update Syncthing device ID

--- src/lib/mirror.sh
+++ src/lib/mirror.sh
@@ -9,7 +9,7 @@ function help-mirror() {
   'syncthing')
     echo "Sorry, I haven't automated this yet!"
     echo '1) Install syncthing'
-    echo '2) Add my device: 4HGSY2A-JLFMQFF-GZWBRJP-DHICO4G-55SJODJ-7E2OHCR-M4Y7EJ3-2I4YOAJ'
+    echo '2) Add my device: ZDHVMSP-EW4TMWX-DBH2W4P-HV5A6OY-BBEFABO-QTENANJ-RJ6GKNX-6KCG7QY'
     echo '3) Add folder "jhcrt-m2dra" as "Receive Only" with "Ignore Permissions" and "Pull Order" by "Oldest First".'
     echo '4) Wait for me to accept it!'
     ;;
