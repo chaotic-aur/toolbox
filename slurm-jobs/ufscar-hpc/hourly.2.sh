183b698e44e0a110a3963b2b156c072965652c50
PedroHLC <root@pedrohlc.com>
2021-05-05 08:51:44 -0300

    Add second node hourly task to UFSCar

---
+++ slurm-jobs/ufscar-hpc/hourly.2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=hourly.2
+#SBATCH --cpus-per-task 40
+#SBATCH --nodes 1
+#SBATCH --exclusive
+#SBATCH --dependency=singleton
+#SBATCH --time=00:50:00
+#SBATCH --signal=B:SIGUSR1@90
+
+# shellcheck disable=SC2034
+JOB_PERIOD=3600
+# shellcheck disable=SC2034
+JOB_OFFSET=0
+
+export CAUR_PARALLEL=10
+
+# shellcheck source=/dev/null
+source common.sh
