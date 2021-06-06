9c6653703615030abec118ccfdb9ec5a3cf03b8e
PedroHLC <root@pedrohlc.com>
2021-06-06 16:49:07 -0300

    Hotfix: "--" kills makepwd

--- src/lib/package-makedir.sh
+++ src/lib/package-makedir.sh
@@ -33,8 +33,9 @@ function makepwd() {
   for _pkg in "${_LS[@]}"; do
     if [[ "$_pkg" == '--' ]]; then
       echo 'Trapped, waiting jobs until here.'
-      wait "${_BUILDING_PIDS[@]}"
+      [[ -n "${_BUILDING_PIDS}" ]] && wait "${_BUILDING_PIDS[@]}" || true
       _BUILDING_PIDS=()
+      echo 'Keep going...'
     elif [[ "${_MAX_JOBS}" == '1' ]]; then
       pipepkg "$_pkg"
     else
