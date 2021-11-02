164eaaf1331c95088980011fd04b67fa229a34fb
dr460nf1r3 <njcrypted@protonmail.com>
2021-11-02 10:34:46 +0100

    Integrate maintainer utils (first try)

--- src/chaotic.sh
+++ src/chaotic.sh
@@ -164,6 +164,9 @@ function main() {
   'send-log' | 'al')
     send-log "${@:2}"
     ;;
+  'check-dup' | 'cd')
+    check-duplicates "${@:2}"
+    ;;
   'whoami')
     echo "#$UID or ${USER:-$(whoami)}, identified as ${CAUR_MAINTAINER} at \"$CAUR_DEPLOY_LABEL\"."
     ;;
