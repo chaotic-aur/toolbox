0840eeb1e8399dfe31c264912b0a72ab6cd7546c
PedroHLC <root@pedrohlc.com>
2021-11-08 18:07:13 -0300

    Newer futex for more fun....

--- src/lib/routines-tkg.sh
+++ src/lib/routines-tkg.sh
@@ -77,6 +77,7 @@ function tkg-kernel-variate() {
   s/_zfsfix=\"[^\"]*\"/_zfsfix=\"true\"/g
   s/_fsync=\"[^\"]*\"/_fsync=\"true\"/g
   s/_futex2=\"[^\"]*\"/_futex2=\"true\"/g
+  s/_futex_waitv=\"[^\"]*\"/_futex_waitv=\"true\"/g
   s/_winesync=\"[^\"]*\"/_winesync=\"false\"/g
   s/_anbox=\"[^\"]*\"/_anbox=\"true\"/g
   s/_processor_opt=\"[^\"]*\"/_processor_opt=\"${_MARCH}\"/g
