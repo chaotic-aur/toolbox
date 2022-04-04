202291f4d560d81eed7015b1a8848b0aab6a36cd
dr460nf1r3 <njcrypted@protonmail.com>
2022-04-04 16:16:58 +0200

    Exit code 38

--- src/lib/utils.sh
+++ src/lib/utils.sh
@@ -130,7 +130,7 @@ function sort-logs() (
 
   if ! [[ -d "$CAUR_LOG_CACHE" ]]; then
     echo 'It was not possible to create a temporary directory for this action.'
-    return 62 # Nico, find the next number, and put it here, I lost count already.
+    return 38
   fi
 
   function cleanup-logs() {
