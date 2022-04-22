a041ef214362746389028f9dc1831c865389f123
TNE <tne@garudalinux.org>
2022-04-22 02:30:42 +0200

    Improve chaotic get
    Chaotic get will now check the package lists to check if there is another source available for the package other than the aur. Sometimes we use external git sources directly instead of the aur, and this makes us download the correct package.

--- src/lib/aur.sh
+++ src/lib/aur.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
 function aur-download() {
-  repoctl down "$@"
+  set -euo pipefail
+
+  local _LIST _PACKAGES _PKGBASE _REPOCTL _GIT _OUT
+  _REPOCTL=()
+  declare -A _GIT
+  _PACKAGES="$(find "${CAUR_PACKAGE_LISTS}" -name '*.txt' | while read -r _LIST; do parse-package-list "$_LIST"; done)"
+  for _PKGBASE in "$@"; do
+    _OUT="$(awk -F ':' -v pkgbase="${_PKGBASE}" '$1==pkgbase { OFS = FS; $1=""; print substr($0, 2); exit }' <<<"$_PACKAGES")"
+    if [[ -z "${_OUT}" ]]; then
+      _REPOCTL+=("${_PKGBASE}")
+    else
+      _GIT["${_PKGBASE}"]="${_OUT}"
+    fi
+  done
+
+  if [ ${#_REPOCTL[@]} -ne 0 ]; then
+    repoctl down "${_REPOCTL[@]}"
+  fi
+  for _PKGBASE in "${!_GIT[@]}"; do
+    git clone --depth 1 "${_GIT[${_PKGBASE}]}" "${_PKGBASE}"
+  done
 }
