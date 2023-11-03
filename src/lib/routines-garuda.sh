c9351b907a11dfbe5c3c852948d3415d6d933cfb
Nico <github@dr460nf1r3.org>
2023-11-03 14:15:42 +0000

    feat: add Garuda routine (#98)
    * feat: add Garuda routine
    
    * style: comply to Prettier

---
+++ src/lib/routines-garuda.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function routine-garuda() {
+  set -euo pipefail
+  clean-xdg
+  interfere-sync
+  push-routine-dir 'garuda'
+
+  [[ -d "_repo" ]] && rm -rf --one-file-system '_repo'
+  git clone 'https://gitlab.com/garuda-linux/pkgbuilds.git' '_repo'
+  mv _repo/* .
+
+  (makepwd) || true
+
+  clean-logs
+  popd #routine-dir
+  return 0
+}
