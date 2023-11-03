bf6c2be8d332ac0b6c9dbbc30c2cdb4ec9f58f08
Nico Jensch <root@dr460nf1r3.org>
2023-11-03 12:11:25 +0100

    style: comply to Prettier

--- src/lib/routines-garuda.sh
+++ src/lib/routines-garuda.sh
@@ -9,7 +9,7 @@ function routine-garuda() {
   [[ -d "_repo" ]] && rm -rf --one-file-system '_repo'
   git clone 'https://gitlab.com/garuda-linux/pkgbuilds.git' '_repo'
   mv _repo/* .
-  
+
   (makepwd) || true
 
   clean-logs
