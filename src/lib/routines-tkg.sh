56efc38296f6187549421cae20a92123110ca3d3
Nico Jensch <root@dr460nf1r3.org>
2024-03-25 12:30:40 +0100

    feat(tkg): bring 6.8 to the party

--- src/lib/routines-tkg.sh
+++ src/lib/routines-tkg.sh
@@ -106,7 +106,7 @@ function tkg-kernels-variations() {
   local _LINUX_LTS _LINUX_STABLE _LINUX_MARCH _VAR_SCHED _VAR_SCHED
 
   _LINUX_LTS='6.6'
-  _LINUX_STABLE='6.7'
+  _LINUX_STABLE='6.8'
 
   _LINUX_SCHED=(
     'bmq 1'
